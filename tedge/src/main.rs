bc407b91f614d2acb7db914d9b1299fdc9e99d08
Matthias Beyer <matthias.beyer@ifm.com>
2022-03-21 14:42:13 +0100

    Remove plugin_log
    Remove implementation, because the log plugin does not have a usecase
    anymore, not even for debugging.
    
    Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>
    Signed-off-by: Marcel Müller <m.mueller@ifm.com>

--- tedge/src/main.rs
+++ tedge/src/main.rs
@@ -63,11 +63,6 @@ async fn main() -> anyhow::Result<()> {
     );
     let application = register_plugin!(
         application,
-        "builtin_plugin_log",
-        plugin_log::LogPluginBuilder
-    );
-    let application = register_plugin!(
-        application,
         "builtin_plugin_sysstat",
         plugin_sysstat::SysStatPluginBuilder
     );
