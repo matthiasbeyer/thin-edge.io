09439de0a6fbfe8dd5032f48045f89067ed66c68
Matthias Beyer <matthias.beyer@ifm.com>
2022-05-11 11:19:59 +0200

    Make logging non-optional
    Before this patch, the logging code was optional in tedge-cli. But we
    always want to build this piece of code.
    
    Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

--- tedge/src/logging.rs
+++ tedge/src/logging.rs
@@ -1,25 +1,19 @@
-#[allow(unused)]
 pub(crate) fn setup_logging(verbose: bool, debugging: bool) -> miette::Result<()> {
-    #[cfg(feature = "core_logging")]
-    {
-        use tracing_subscriber::filter::EnvFilter;
-        use tracing_subscriber::filter::LevelFilter;
+    use tracing_subscriber::filter::EnvFilter;
+    use tracing_subscriber::filter::LevelFilter;
 
-        let filter = if verbose && !debugging {
-            EnvFilter::from_default_env().add_directive(LevelFilter::INFO.into())
-        } else if debugging {
-            EnvFilter::from_default_env().add_directive(LevelFilter::DEBUG.into())
-        } else {
-            EnvFilter::from_default_env()
-        };
+    let filter = if verbose && !debugging {
+        EnvFilter::from_default_env().add_directive(LevelFilter::INFO.into())
+    } else if debugging {
+        EnvFilter::from_default_env().add_directive(LevelFilter::DEBUG.into())
+    } else {
+        EnvFilter::from_default_env()
+    };
 
-        let subscriber = tracing_subscriber::fmt::Subscriber::builder()
-            .with_env_filter(filter)
-            .finish();
+    let subscriber = tracing_subscriber::fmt::Subscriber::builder()
+        .with_env_filter(filter)
+        .finish();
 
-        tracing::subscriber::set_global_default(subscriber)
-            .map_err(|e| miette::miette!("Failed to set global subscriber: {:?}", e))?;
-    }
-
-    Ok(())
+    tracing::subscriber::set_global_default(subscriber)
+        .map_err(|e| miette::miette!("Failed to set global subscriber: {:?}", e))
 }
