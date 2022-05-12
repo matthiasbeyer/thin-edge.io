755e512e03f19485308a3982b845b675691bc37b
Matthias Beyer <matthias.beyer@ifm.com>
2022-05-12 12:23:34 +0200

    plugin_avg: Make config type self-describing
    This patch rewrites the configuration type to be self-describing and
    adapts the `PluginBuilder` implementation to implement the
    `PluginBuilder::kind_configuration()` interface.
    
    Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

--- plugins/plugin_avg/src/lib.rs
+++ plugins/plugin_avg/src/lib.rs
@@ -20,11 +20,12 @@ use tokio::sync::RwLock;
 
 pub struct AvgPluginBuilder;
 
-#[derive(serde::Deserialize, Debug)]
+#[derive(serde::Deserialize, Debug, tedge_api::Config)]
 struct AvgConfig {
-    #[serde(with = "humantime_serde")]
-    timeframe: std::time::Duration,
+    /// The duration of the time window to calculate the average for
+    timeframe: tedge_lib::config::Humantime,
 
+    /// The name of the plugin to send the result to
     target: String,
 
     /// Whether to report a 0 (zero) if there are zero measurements in the timeframe
@@ -44,6 +45,10 @@ enum Error {
 impl<PD: PluginDirectory> PluginBuilder<PD> for AvgPluginBuilder {
     fn kind_name() -> &'static str {
         "avg"
+    }
+
+    fn kind_configuration() -> Option<tedge_api::ConfigDescription> {
+        Some(<AvgConfig as tedge_api::AsConfig>::as_config())
     }
 
     async fn verify_configuration(
@@ -119,7 +124,7 @@ impl Plugin for AvgPlugin {
             values: self.values.clone(),
         };
         let (stopper, mainloop) =
-            tedge_lib::mainloop::Mainloop::ticking_every(self.config.timeframe, state);
+            tedge_lib::mainloop::Mainloop::ticking_every(self.config.timeframe.into_duration(), state);
         self.stopper = Some(stopper);
 
         let _ = tokio::spawn(mainloop.run(main_avg));
