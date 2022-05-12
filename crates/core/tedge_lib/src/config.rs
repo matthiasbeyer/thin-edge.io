d51398ee446c0754882af4a0b0ad1d9c24599c5f
Matthias Beyer <matthias.beyer@ifm.com>
2022-05-12 12:19:54 +0200

    Add helper type for Humantime configuration members
    This patch adds a helper type for having a human-readable duration in
    the configuration of a plugin.
    This helper type implements Deserialize with the humantime crate, so
    that durations can be specified as (e.g.) "5mins".
    
    The AsConfig trait is implemented so that it can be used in
    self-describing configuration types.
    
    Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

---
+++ crates/core/tedge_lib/src/config.rs
@@ -0,0 +1,56 @@
+#[derive(Copy, Clone, Debug, serde::Deserialize)]
+#[serde(transparent)]
+pub struct Humantime(#[serde(with = "humantime_serde")] std::time::Duration);
+
+impl Humantime {
+    pub fn into_duration(self) -> std::time::Duration {
+        self.0
+    }
+}
+
+impl tedge_api::AsConfig for Humantime {
+    fn as_config() -> tedge_api::ConfigDescription {
+        tedge_api::ConfigDescription::new(
+            "Duration-representing String".to_string(),
+            tedge_api::ConfigKind::String,
+            Some(indoc::indoc!{r#"
+                A String that represents a duration
+
+                ## Examples
+
+                A duration of one minute:
+
+                ```toml
+                "1min"
+                ```
+
+                A duration of 5 minutes and 2 nanoseconds:
+
+                ```toml
+                "5min 2ns"
+                ```
+
+                ## More information
+
+                For more information have a look at the documentation of
+                [the humantime crate](https://docs.rs/humantime).
+            "#}),
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_humantime_deser() {
+        let ts = r#"t = "1sec""#;
+
+        #[derive(serde::Deserialize)]
+        struct T {
+            t: super::Humantime,
+        }
+
+        let ht: T = toml::from_str(ts).unwrap();
+        assert_eq!(ht.t.0, std::time::Duration::from_secs(1));
+    }
+}
