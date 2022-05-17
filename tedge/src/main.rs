02aebf91f9ccb195e13a11905e5233407fa4f99b
Matthias Beyer <matthias.beyer@ifm.com>
2022-05-17 13:52:58 +0200

    Wire up c8y plugin in tedge-cli main()
    Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

--- tedge/src/main.rs
+++ tedge/src/main.rs
@@ -162,6 +162,12 @@ async fn main() -> miette::Result<()> {
         plugin_notification::NotificationPluginBuilder,
         plugin_notification::NotificationPluginBuilder
     );
+    let registry = register_plugin!(
+        registry,
+        "c8y",
+        plugin_c8y::C8yPluginBuilder,
+        plugin_c8y::C8yPluginBuilder
+    );
 
     match args.command {
         cli::CliCommand::Run { config } => {
