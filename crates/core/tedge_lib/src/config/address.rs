2a4cea7e69b44411febc5675681eaefa2b8e159b
Matthias Beyer <matthias.beyer@ifm.com>
2022-05-18 15:10:58 +0200

    Add test for deserializing list of addresses
    Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

--- crates/core/tedge_lib/src/config/address.rs
+++ crates/core/tedge_lib/src/config/address.rs
@@ -103,6 +103,11 @@ mod tests {
         addr: super::Address,
     }
 
+    #[derive(serde::Deserialize)]
+    struct G {
+        addrs: crate::config::OneOrMany<super::Address>,
+    }
+
     #[test]
     fn test_deser() {
         let s = r#"
@@ -111,5 +116,19 @@ mod tests {
 
         let a: A = toml::from_str(s).unwrap();
         assert_eq!(a.addr.0, "foo");
+    }
+
+    #[test]
+    fn test_deser_group() {
+        let s = r#"
+            addrs = [ "foo", "bar", "baz" ]
+        "#;
+
+        let g: G = toml::from_str(s).unwrap();
+        let addrs = g.addrs.into_vec();
+        assert_eq!(addrs.get(0).map(AsRef::as_ref), Some("foo"));
+        assert_eq!(addrs.get(1).map(AsRef::as_ref), Some("bar"));
+        assert_eq!(addrs.get(2).map(AsRef::as_ref), Some("baz"));
+        assert_eq!(addrs.get(3), None);
     }
 }
