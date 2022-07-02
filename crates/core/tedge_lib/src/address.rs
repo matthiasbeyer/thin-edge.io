9675d2a314901e8cb355082a8c21d40f367f45d9
Matthias Beyer <matthias.beyer@ifm.com>
2022-07-02 15:33:33 +0200

    Add documentation for AddressGroup type
    Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

--- crates/core/tedge_lib/src/address.rs
+++ crates/core/tedge_lib/src/address.rs
@@ -4,6 +4,13 @@ use futures::StreamExt;
 
 use crate::config::OneOrMany;
 
+/// A collection of addresses
+///
+/// This type implements helper functionality for a list of addresses.
+/// If the user can specify one or many addresses (via the
+/// [OneOrMany](crate::config::OneOrMany) config helper type)
+/// they can be collected into an [AddressGroup](crate::address::AddressGroup)
+/// and be used together with some convenience for sending to all addresses at once.
 #[derive(Debug)]
 pub struct AddressGroup<RB: tedge_api::address::ReceiverBundle>(Vec<tedge_api::Address<RB>>);
 
@@ -11,6 +18,13 @@ impl<RB> AddressGroup<RB>
 where
     RB: tedge_api::address::ReceiverBundle,
 {
+    /// Build an [AddressGroup] from [OneOrMany](crate::config::OneOrMany)
+    /// [Address](crate::config::Address)es.
+    ///
+    /// This function fails with [AddressGroupBuildError] containing all errors that were collected
+    /// when asking the [PluginDirectory](tedge_api::PluginDirectory::get_address_for) (via
+    /// [Address::build](crate::config::Address::build)) for the [Address](tedge_api::Address)
+    /// object.
     pub fn build<PD>(
         pd: &PD,
         addrs: &OneOrMany<crate::config::Address>,
@@ -41,6 +55,16 @@ where
         }
     }
 
+    /// Send a [Message](tedge_api::Message) to all addresses in the group
+    ///
+    /// This function internally uses [Address::send_and_wait](tedge_api::Address::send_and_wait).
+    ///
+    /// # Note
+    ///
+    /// This function returns a stream that must be consumed to actually send the messages out.
+    ///
+    /// The caller of this function can, using the returned stream, decide whether the messages
+    /// should be sent out concurrently or in sequence.
     pub fn send_and_wait<M>(
         &self,
         msg: M,
@@ -52,6 +76,16 @@ where
         futures::stream::iter(&self.0).then(move |addr| addr.send_and_wait(msg.clone()))
     }
 
+    /// Try to send a [Message](tedge_api::Message) to all addresses in the group
+    ///
+    /// This function internally uses [Address::try_send](tedge_api::Address::try_send).
+    ///
+    /// # Note
+    ///
+    /// This function returns a stream that must be consumed to actually send the messages out.
+    ///
+    /// The caller of this function can, using the returned stream, decide whether the messages
+    /// should be sent out concurrently or in sequence.
     pub fn try_send<M>(
         &self,
         msg: M,
