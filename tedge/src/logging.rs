49526c5907a901f0597dd734627a62af6bc73175
Matthias Beyer <matthias.beyer@ifm.com>
2022-07-06 11:17:07 +0200

    Replace manual map with Option::map()
    Fixes lint: clippy::manual_map
    
    Signed-off-by: Matthias Beyer <matthias.beyer@ifm.com>

--- tedge/src/logging.rs
+++ tedge/src/logging.rs
@@ -61,11 +61,7 @@ pub fn setup_logging(
 
     let opt_tracy_layer = tracy_logging.then(|| tracing_tracy::TracyLayer::new());
 
-    let stdout_log = if let Some(env_filter) = env_filter {
-        Some(tracing_subscriber::fmt::layer().with_filter(env_filter))
-    } else {
-        None
-    };
+    let stdout_log = env_filter.map(|f| tracing_subscriber::fmt::layer().with_filter(f));
 
     let subscriber = tracing_subscriber::registry::Registry::default()
         .with(opt_chrome_layer)
